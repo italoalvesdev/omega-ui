983ff65ff02b3b69ea0f9ca70b57d601b46a2a35
Italo Alves <italo_silva007@hotmail.com>
2022-11-22 00:03:39 -0300

    ci: Remove vite configuration

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -16,11 +16,11 @@ module.exports = {
   "features": {
     "storyStoreV7": true
   },
-  viteFinal: (config, { configType }) => {
-    if (configType === 'PRODUCTION') {
-      config.base = '/omega/'
-    }
+  // viteFinal: (config, { configType }) => {
+  //   if (configType === 'PRODUCTION') {
+  //     config.base = '/omega/'
+  //   }
 
-    return config
-  }
+  //   return config
+  // }
 }
