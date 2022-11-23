ccc941099d0c38dd42e3f52e467f490fe98f767c
Italo Alves <italo_silva007@hotmail.com>
2022-11-23 01:16:13 -0300

    styles: Add responsive layout all stories to Storybook

---
+++ packages/docs/.storybook/preview.jsx
@@ -0,0 +1,30 @@
+import { themes } from "@storybook/theming";
+
+export const decorators = [
+  (Story) => (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "100%",
+        height: "100%",
+      }}
+    >
+      <Story />
+    </div>
+  ),
+];
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  docs: {
+    theme: themes.dark,
+  },
+};
